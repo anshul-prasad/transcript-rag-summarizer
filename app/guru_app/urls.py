343884f8d57447b5ac2f7b41c4f101d43fa54a78
Anshul Prasad <anshulprasad5440.com>
2026-09-24 10:59:22 +0530

    files moved to root folder "app"

--- app/guru_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('ask/', views.ask, name='ask'),
-    path('history/', views.history, name='history'),
-    path('clear-history/', views.clear_history, name='clear_history'),
-]
